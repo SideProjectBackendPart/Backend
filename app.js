19b5f67faa4240d5e1b5b414dea8cf938a2089eb
jimin <coolcats6762@gmail.com>
2024-12-21 23:15:15 +0900

    cors 처리리

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const fs = require('fs');
 const path = require('path');
 
 const port = 8000;
+const cors = require('cors')
+
+let corsOptions = {
+  origin: '*',      // 출처 허용 옵션
+  credential: true, // 사용자 인증이 필요한 리소스(쿠키 등) 접근
+}
+
+app.use(cors(corsOptions));
 
 app.set('views', path.join(__dirname, 'views')); // views 폴더 경로 설정
 app.set('view engine', 'ejs'); // EJS를 기본 템플릿 엔진으로 설정
