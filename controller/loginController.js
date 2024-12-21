a317458e910c32bd96237079df4de5c0914a3097
jimin <coolcats6762@gmail.com>
2024-12-21 23:35:01 +0900

    cors 에러 처리

--- controller/loginController.js
+++ controller/loginController.js
@@ -13,30 +13,40 @@ async function login(req, res) {
     console.log(req.body.email);
     console.log(req.body.password);
 
-    let result = await LoginMapper.checkMember(req.body.email);
-    console.log("result", result[0])
-    if (result[0].password == req.body.password) {
-        // 로그인 성공 예제 데이터를 작성
-        const response = {
-            success: true,
-            message: "Login successful",   // 로그인 성공 메시지
-            token: "your-jwt-token",       // 인증에 사용할 JWT 토큰
-            user: {
-                id: result[0].user_id,             // 사용자 ID
-                name: result[0].name,         // 사용자 이름
-                email: result[0].email       // 사용자 이메일
-            }
-        };
+    try {
+        let result = await LoginMapper.checkMember(req.body.email);
+        console.log("result", result[0])
+        if (result[0].password == req.body.password) {
+            // 로그인 성공 예제 데이터를 작성
+            const response = {
+                success: true,
+                message: "Login successful",   // 로그인 성공 메시지
+                token: "your-jwt-token",       // 인증에 사용할 JWT 토큰
+                user: {
+                    id: result[0].user_id,             // 사용자 ID
+                    name: result[0].name,         // 사용자 이름
+                    email: result[0].email       // 사용자 이메일
+                }
+            };
 
-        // 클라이언트에 JSON 응답 전송
-        res.json(response);
-    } else {
+            // 클라이언트에 JSON 응답 전송
+            res.json(response);
+        } else {
+            res.json({
+                success: false,
+                message: "Invalid credentials",
+
+            });
+        }
+    } catch {
         res.json({
             success: false,
             message: "Invalid credentials",
 
         });
     }
+
+
 }
 
 async function register(req, res) {
@@ -67,7 +77,7 @@ async function register(req, res) {
         email: req.body.email,
         password: req.body.password,
         birth_date: req.body.birth_date,
-        
+
         user_name: req.body.profile.name,
         nickname: req.body.profile.nickname,
         profile_image_url: req.body.profile.profile_image_url,
